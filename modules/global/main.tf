c72060bce4ef640d1926af41f6dc6a941a0b2dfc
ipatillon@expertime.com <ipatillon@expertime.com>
2022-10-24 14:35:46 +0200

    local run

--- modules/global/main.tf
+++ modules/global/main.tf
@@ -1,8 +1,8 @@
 
 output "naming" {
     value = {
-        sqlserver1 			= "sql-pp-db-1"
-        store1 			    = "stojpaduxe34561"
+        sqlserver1			= "sql-pp-db-1"
+        store1				= "stojpaduxe34561"
         webapp01			= "DEFAULTwebapp01"
     }
 }
